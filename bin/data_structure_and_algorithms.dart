9460da5efc52c30f342b72e6cbe23671bc9cb050
Mostafa <mostafa.saad.emara@gmail.com>
2022-12-09 06:21:46 +0200

    _merge iterable parameters

--- bin/data_structure_and_algorithms.dart
+++ bin/data_structure_and_algorithms.dart
@@ -1,7 +1,6 @@
-import 'package:data_structure_and_algorithms/selection_sort.dart';
+import 'package:data_structure_and_algorithms/merge_sort.dart';
 
 void main(List<String> arguments) {
   final list = [10, 6, 3, 12, 5, 1, 7, 4, 2, 8, 9];
-  selectionSort(list);
-  print(list);
+  print(mergeSort(list));
 }
--- lib/merge_sort.dart
+++ lib/merge_sort.dart
@@ -4,7 +4,7 @@ List<E> mergeSort<E extends Comparable<dynamic>>(List<E> list) {
   final middle = list.length ~/ 2;
   final left = mergeSort(list.sublist(0, middle));
   final right = mergeSort(list.sublist(middle));
-  return _merge(left, right);
+  return _mergeIterable(left, right);
 }
 
 List<E> _merge<E extends Comparable<dynamic>>(List<E> listA, List<E> listB) {
@@ -34,6 +34,43 @@ List<E> _merge<E extends Comparable<dynamic>>(List<E> listA, List<E> listB) {
 
   if (indexB < listB.length) {
     result.addAll(listB.getRange(indexB, listB.length));
+  }
+  return result;
+}
+
+//challenge
+List<E> _mergeIterable<E extends Comparable<dynamic>>(
+    Iterable<E> collectionA, Iterable<E> collectionB) {
+  var indexA = 0;
+  var indexB = 0;
+  final result = <E>[];
+  while (indexA < collectionA.length && indexB < collectionB.length) {
+    final valueA = collectionA.elementAt(indexA);
+    final valueB = collectionB.elementAt(indexB);
+    if (valueA.compareTo(valueB) < 0) {
+      result.add(valueA);
+      indexA += 1;
+    } else if (valueA.compareTo(valueB) > 0) {
+      result.add(valueB);
+      indexB += 1;
+    } else {
+      result.add(valueA);
+      result.add(valueB);
+      indexA += 1;
+      indexB += 1;
+    }
+  }
+
+  if (indexA < collectionA.length) {
+    for (int i = indexA; i < collectionA.length; i++) {
+      result.add(collectionA.elementAt(i));
+    }
+  }
+
+  if (indexB < collectionB.length) {
+    for (int i = indexB; i < collectionB.length; i++) {
+      result.add(collectionB.elementAt(i));
+    }
   }
   return result;
 }
