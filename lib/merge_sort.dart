5ba0562dcc53fb8c95a72800f65ec60783102e36
Mostafa <mostafa.saad.emara@gmail.com>
2022-12-09 06:36:44 +0200

    another merge cllange solution

--- lib/merge_sort.dart
+++ lib/merge_sort.dart
@@ -4,7 +4,7 @@ List<E> mergeSort<E extends Comparable<dynamic>>(List<E> list) {
   final middle = list.length ~/ 2;
   final left = mergeSort(list.sublist(0, middle));
   final right = mergeSort(list.sublist(middle));
-  return _mergeIterable(left, right);
+  return _mergeIterable2(left, right);
 }
 
 List<E> _merge<E extends Comparable<dynamic>>(List<E> listA, List<E> listB) {
@@ -34,6 +34,45 @@ List<E> _merge<E extends Comparable<dynamic>>(List<E> listA, List<E> listB) {
 
   if (indexB < listB.length) {
     result.addAll(listB.getRange(indexB, listB.length));
+  }
+  return result;
+}
+
+//challenge solution 2
+List<E> _mergeIterable2<E extends Comparable<dynamic>>(
+    Iterable<E> collectionA, Iterable<E> collectionB) {
+  var currentElementA = collectionA.iterator;
+  var currentElementB = collectionB.iterator;
+  var firstHasValue = currentElementA.moveNext();
+  var secondHasValue = currentElementB.moveNext();
+  final result = <E>[];
+  while (firstHasValue && secondHasValue) {
+    final valueA = currentElementA.current;
+    final valueB = currentElementB.current;
+    if (valueA.compareTo(valueB) < 0) {
+      result.add(valueA);
+      firstHasValue = currentElementA.moveNext();
+    } else if (valueA.compareTo(valueB) > 0) {
+      result.add(valueB);
+      secondHasValue = currentElementB.moveNext();
+    } else {
+      result.add(valueA);
+      result.add(valueB);
+      firstHasValue = currentElementA.moveNext();
+      secondHasValue = currentElementB.moveNext();
+    }
+  }
+
+  if (firstHasValue) {
+    do {
+      result.add(currentElementA.current);
+    } while (currentElementA.moveNext());
+  }
+
+  if (secondHasValue) {
+    do {
+      result.add(currentElementB.current);
+    } while (currentElementB.moveNext());
   }
   return result;
 }
