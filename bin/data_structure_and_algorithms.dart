6313d984c999f5912b7e359a81a942a7127b07cd
Mostafa <mostafa.saad.emara@gmail.com>
2023-01-18 07:23:18 +0200

    implement Adjacency List Graph

--- bin/data_structure_and_algorithms.dart
+++ bin/data_structure_and_algorithms.dart
@@ -1,8 +1,27 @@
-import 'package:data_structure_and_algorithms/quick_sort.dart';
+import 'package:data_structure_and_algorithms/graph.dart';
 
 void main(List<String> arguments) {
-  final list = [6, 12, 2, 26, 8, 18, 21, 9, 51];
-  quickSortLomutoIter(list, 0, list.length - 1);
+  final graph = AdjacencyList<String>();
+  final singapore = graph.createVertex('Singapore');
+  final tokyo = graph.createVertex('Tokyo');
+  final hongKong = graph.createVertex('HongKong');
+  final detroit = graph.createVertex('Detroit');
+  final sanFrancisco = graph.createVertex('SanFrancisco');
+  final washingtonDc = graph.createVertex('Washington Dc');
+  final austinTexas = graph.createVertex('Austin Texas');
+  final seattle = graph.createVertex('Seattle');
 
-  print(list);
+  graph.addEdge(singapore, hongKong, weight: 300);
+  graph.addEdge(singapore, tokyo, weight: 500);
+  graph.addEdge(hongKong, tokyo, weight: 250);
+  graph.addEdge(tokyo, detroit, weight: 450);
+  graph.addEdge(tokyo, washingtonDc, weight: 300);
+  graph.addEdge(hongKong, sanFrancisco, weight: 600);
+  graph.addEdge(detroit, austinTexas, weight: 50);
+  graph.addEdge(austinTexas, washingtonDc, weight: 292);
+  graph.addEdge(sanFrancisco, washingtonDc, weight: 337);
+  graph.addEdge(washingtonDc, seattle, weight: 277);
+  graph.addEdge(sanFrancisco, seattle, weight: 218);
+  graph.addEdge(austinTexas, sanFrancisco, weight: 297);
+  print(graph);
 }
--- lib/graph.dart
+++ lib/graph.dart
@@ -29,3 +29,55 @@ abstract class Graph<E> {
     Vertex<E> destination,
   );
 }
+
+class AdjacencyList<E> implements Graph<E> {
+  final Map<Vertex<E>, List<Edge<E>>> _connections = {};
+  var _nextIndex = 0;
+  @override
+  // TODO: implement vertices
+  Iterable<Vertex<E>> get vertices => _connections.keys;
+  @override
+  void addEdge(Vertex<E> source, Vertex<E> destination,
+      {EdgeType edgeType = EdgeType.undirected, double? weight}) {
+    _connections[source]
+        ?.add(Edge(source: source, destination: destination, weight: weight));
+    if (edgeType == EdgeType.undirected) {
+      _connections[destination]
+          ?.add(Edge(source: destination, destination: source, weight: weight));
+    }
+  }
+
+  @override
+  Vertex<E> createVertex(E data) {
+    final vertex = Vertex(index: _nextIndex, data: data);
+
+    _nextIndex += 1;
+    _connections[vertex] = [];
+    return vertex;
+  }
+
+  @override
+  List<Edge<E>> edges(Vertex<E> source) {
+    return _connections[source] ?? [];
+  }
+
+  @override
+  double? weight(Vertex<E> source, Vertex<E> destination) {
+    final match =
+        edges(source).where((element) => element.destination == destination);
+    if (match.isEmpty) return null;
+    return match.first.weight;
+  }
+
+  @override
+  String toString() {
+    final result = StringBuffer();
+    _connections.forEach((vertex, edges) {
+      final destinations = edges.map((edge) {
+        return edge.destination;
+      }).join(', ');
+      result.writeln("$vertex --->$destinations");
+    });
+    return result.toString();
+  }
+}
